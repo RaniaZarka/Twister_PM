b857477665e6407062faaa6fcda3efa8cb9a0bf8
RaniaZarka <raniazarka@hotmail.com>
2021-03-26 08:51:19 +0100

    small changes in UI in all activities

--- app/src/main/java/com/example/twisterpm/MainActivity.java
+++ app/src/main/java/com/example/twisterpm/MainActivity.java
@@ -3,14 +3,15 @@ package com.example.twisterpm;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
@@ -20,12 +21,24 @@ import com.google.firebase.auth.FirebaseUser;
 public class MainActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
 
+    private static  final String TAG ="autoSignin";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 // Initialize Firebase Auth
         mAuth = FirebaseAuth.getInstance();
+      /*  FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            // User is signed in
+            Intent i = new Intent(MainActivity.this, AllMessagesActivity.class);
+            i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(i);
+        } else {
+            // User is signed out
+            Log.d(TAG, "onAuthStateChanged:signed_out");
+        }*/
     }
 
     // we can have all these in the onStart method or we can create a login method
@@ -35,15 +48,22 @@ public class MainActivity extends AppCompatActivity {
         // Check if user is signed in (non-null) and update UI accordingly.
         FirebaseUser currentUser = mAuth.getCurrentUser();
         Log.d("UserApple", "Current user " + currentUser);
+        /*if(currentUser !=null){
+            reload();
+        }*/
+    }
+
+
+    public void hideKeyboard(View view) {
+        InputMethodManager inputMethodManager =(InputMethodManager)getSystemService(Activity.INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
 
     public void login(View view) {
-
         EditText emailView = findViewById(R.id.mainEmail);
         EditText paswordView = findViewById(R.id.mainPasswordInput);
         String email = emailView.getText().toString().trim();
         String password = paswordView.getText().toString().trim();
-
         if (email.isEmpty() || password.isEmpty()) {
             Toast.makeText(getApplicationContext(), "All fields must be filled", Toast.LENGTH_LONG).show();
         } else {
@@ -79,6 +99,7 @@ public class MainActivity extends AppCompatActivity {
         String email = emailView.getText().toString().trim();
         String password = paswordView.getText().toString().trim();
 
+        
         if (email.isEmpty() || password.isEmpty()) {
             Toast.makeText(getApplicationContext(), "All fields must be filled", Toast.LENGTH_LONG).show();
         } else {
